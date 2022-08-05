20085cf2a5584d1c97bf7e17b968074a15edc501
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-08-04 23:25:56 -0300

    feat: adicao de validacao de desigualdade triangular

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ struct Triangulo {
 
 impl Triangulo {
     fn new(a: i32, b: i32, c: i32) -> Triangulo {
+        if ((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a) {
+            panic!("Se a soma entre os dois lados é igual ou menor ao terceiro, esse triângulo não pode existir.")
+        }
+
         Triangulo { a, b, c }
     }
 }
@@ -15,11 +19,17 @@ mod tests {
     use crate::Triangulo;
 
     #[test]
-    fn test_criar_triangulo() {
+    fn test_sucesso_criacao_triangulo() {
         let triangulo = Triangulo::new(16, 20, 30);
         
         assert_eq!(triangulo.a, 16);
         assert_eq!(triangulo.b, 20);
         assert_eq!(triangulo.c, 30);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_erro_criacao_triangulo_quando_desigualdade_triangular() {
+        Triangulo::new(20, 15, 5);
     }
 }
