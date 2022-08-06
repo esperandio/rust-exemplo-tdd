9a52c5b6cd7036c7da6115c637120df20b27c1d5
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-08-06 11:49:14 -0300

    refactor: extracao de codigo referente a struct de Triangulo e movida para um modulo separado

---
+++ src/geometria.rs
@@ -0,0 +1,21 @@
+pub mod formas_geometricas {
+    pub struct Triangulo {
+        a: i32,
+        b: i32,
+        c: i32
+    }
+
+    impl Triangulo {
+        pub fn new(a: i32, b: i32, c: i32) -> Triangulo {
+            if ((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a) {
+                panic!("Se a soma entre os dois lados é igual ou menor ao terceiro, esse triângulo não pode existir.")
+            }
+    
+            Triangulo { a, b, c }
+        }
+
+        pub fn equals(&self, a: i32, b: i32, c: i32) -> bool {
+            self.a == a && self.b == b && self.c == c
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,8 @@
-mod geometria {
-    pub struct Triangulo {
-        a: i32,
-        b: i32,
-        c: i32
-    }
+pub mod geometria;
 
-    impl Triangulo {
-        pub fn new(a: i32, b: i32, c: i32) -> Triangulo {
-            if ((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a) {
-                panic!("Se a soma entre os dois lados é igual ou menor ao terceiro, esse triângulo não pode existir.")
-            }
-    
-            Triangulo { a, b, c }
-        }
-
-        pub fn equals(&self, a: i32, b: i32, c: i32) -> bool {
-            self.a == a && self.b == b && self.c == c
-        }
-    }
-}
 #[cfg(test)]
 mod tests {
-    use crate::geometria::Triangulo;
+    use crate::geometria::formas_geometricas::Triangulo;
 
     #[test]
     fn test_sucesso_criacao_triangulo() {
