689ab11c0a016bb95e6b71309e9bc38b60868e80
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-08-06 11:22:37 -0300

    refactor: criacao de modulo geometria para tornar privada a visibilidade dos campos de Triangulo e deixando publico o metodo construtor (encapsulamento dos dados)

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,37 @@
-struct Triangulo {
-    a: i32,
-    b: i32,
-    c: i32
-}
+mod geometria {
+    pub struct Triangulo {
+        a: i32,
+        b: i32,
+        c: i32
+    }
 
-impl Triangulo {
-    fn new(a: i32, b: i32, c: i32) -> Triangulo {
-        if ((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a) {
-            panic!("Se a soma entre os dois lados é igual ou menor ao terceiro, esse triângulo não pode existir.")
+    impl Triangulo {
+        pub fn new(a: i32, b: i32, c: i32) -> Triangulo {
+            if ((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a) {
+                panic!("Se a soma entre os dois lados é igual ou menor ao terceiro, esse triângulo não pode existir.")
+            }
+    
+            Triangulo { a, b, c }
         }
 
-        Triangulo { a, b, c }
+        pub fn equals(&self, a: i32, b: i32, c: i32) -> bool {
+            self.a == a && self.b == b && self.c == c
+        }
     }
 }
-
 #[cfg(test)]
 mod tests {
-    use crate::Triangulo;
+    use crate::geometria::Triangulo;
 
     #[test]
     fn test_sucesso_criacao_triangulo() {
-        let triangulo = Triangulo::new(16, 20, 30);
-        
-        assert_eq!(triangulo.a, 16);
-        assert_eq!(triangulo.b, 20);
-        assert_eq!(triangulo.c, 30);
+        let a = 16;
+        let b = 20;
+        let c = 30;
+
+        let triangulo = Triangulo::new(a, b, c);
+
+        assert_eq!(triangulo.equals(a, b, c), true);
     }
 
     #[test]
